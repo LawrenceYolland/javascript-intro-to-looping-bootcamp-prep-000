fe784e1d2335b7e287e463f4cb367b0ad0ccb3c4
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 12:54:31 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,7 +24,7 @@ function doWhileLoop(n){
     i = i + 1;
     return i;
   do {
-    
+    console.log("I run once regardless.")
   }
   while (incrementVariable() < n) {
     console.log("I run once regardless.")

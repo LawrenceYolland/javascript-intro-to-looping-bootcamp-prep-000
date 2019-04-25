d91874ae5682b17a0cf140153e6b286d28eb862d
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 12:47:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,18 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++){
     if (i===1){
-      array.push(`I am 1 strange loop.`)
+      array.push(`I am 1 strange loop.`);
     }
     else {
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`);
     }
   }
-  return array
+  return array;
+}
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0){
+    console.log(countdown--);
+  }
 }
